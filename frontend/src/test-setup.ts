218774e578a53fbb3590622980e1e7ce8a93501a
Arup Saha <sahaaup911@gmail.com>
2026-04-18 00:23:32 +0600

    frontend deleted

--- frontend/src/test-setup.ts
+++
@@ -1,9 +0,0 @@
-import '@testing-library/jest-dom'
-
-// Recharts ResponsiveContainer uses ResizeObserver which is not in jsdom.
-// Provide a minimal shim so chart components don't crash in tests.
-global.ResizeObserver = class ResizeObserver {
-  observe() {}
-  unobserve() {}
-  disconnect() {}
-}
